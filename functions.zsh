38e680a0e03d7308d432153518a2b73eeba13069
Collin Stedman <cstedman@princeton>
2015-03-31 04:31:20 -0400

    Gitignore function

--- functions.zsh
+++ functions.zsh
@@ -23,3 +23,28 @@ function precmd() {
     PROMPT=$_OLD_VIRTUAL_PS1
   fi
 }
+
+#
+# Open the .gitignore in the current directory, or prompt the user to create
+# one if it does not already exist
+#
+function gitignore() {
+  setopt local_options no_case_match
+  local GIT_CHECK=$(git rev-parse --show-toplevel 2> /dev/null) 
+  if ! [[ -n $GIT_CHECK ]]; then
+    echo "${fg_bold[red]}Error:${reset_color} Directory is not a git repository"
+    false
+  else
+    if [[ -a $GIT_CHECK/.gitignore ]]; then
+      $EDITOR $GIT_CHECK/.gitignore
+    else
+      local tmp
+      vared -p 'No .gitignore found. Create one? (y/n) ' -c tmp
+      if [[ $tmp =~ "^(y|yes)$" ]]; then
+        echo ".DS_Store\n*.swp" >> $GIT_CHECK/.gitignore
+      else
+        false
+      fi
+    fi
+  fi
+}
