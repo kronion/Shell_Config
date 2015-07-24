2fb9501fb25e76afda98268ba2a8df52c54fe993
Collin Stedman <collin@predata.com>
2015-07-24 13:40:52 -0400

    Testing what happens when command is rerun

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Your working directory is not clean. Please commit/revert all changes and rerun."
+  exit
+fi
 while read i
 do
   local url=$(echo $i | cut -f1 -d,)
